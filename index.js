f93650a1a05d9cb0d69abc46555539a1a59a18a7
Darkweld <Darkweld@users.noreply.github.com>
2017-05-02 10:02:01 +0000

    date fixed

--- index.js
+++ index.js
@@ -10,8 +10,22 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/:value', function(req, res) {
-  var date = Date(req.params.value);
-    res.end(date);
+  var str = req.params.value;
+  var date;
+  var d = new Date(str);
+  if (isNaN(Number(str)) === false) {
+    date = new Date(Number(str));
+    res.end("Unixtime:" + date.toString() 
+    + "<br> Natural language format date:" 
+    + date.toDateString());
+  } else if (isNaN(d.getTime()) === false) {
+    date = new Date(d.getTime());
+    res.end("Unixtime:" + d.getTime().toString() 
+    + "<br> Natural language format date:" 
+    + date.toDateString());
+  }
+
+    res.end("Unixtime:" + null + "<br> Natural language format date:" + null);
 });
 
 app.listen(app.get('port'), function() {
